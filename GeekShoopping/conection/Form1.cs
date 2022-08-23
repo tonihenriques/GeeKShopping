79c19386b9a7a6a1c9e81ede423ab9d316a18aca
tonihenriques <antoniohenriques52@gmail.com>
2022-08-23 08:11:26 -0300

    Product

---
+++ GeekShoopping/conection/Form1.cs
@@ -0,0 +1,10 @@
+namespace conection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GeekShopping.ProductAPI/Context/SqlContext.cs
@@ -0,0 +1,15 @@
+﻿using GeekShopping.ProductAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Context
+{
+    public class SqlContext: DbContext
+    {
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
