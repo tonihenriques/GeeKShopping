9ceea58a7ae714ac25e18acf8b03fb450141bdcd
Antonio Henriques <antoniohenriques52@gmail.com>
2022-09-15 21:36:31 -0300

    Criação do Cart -Carrinho de compras

---
+++ GeekShoopping/GeekShopping.CartAPI/Model/Cart.cs
@@ -0,0 +1,9 @@
+﻿namespace GeekShopping.CartAPI.Model
+{
+    public class Cart
+    {
+        public CartHeader CartHeader { get; set; }
+
+        public IEnumerable<CartDetail> CartDetail { get; set; }
+    }
+}
---
+++ GeekShoopping/GeekShopping.CartAPI/Model/Context/SqlContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CartAPI.Model.Context
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext() {}
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options) {}
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CartDetail> CartDetail { get; set; }
+        public DbSet<CartHeader> CartHeader { get; set; }
+
+       
+    }
+}
