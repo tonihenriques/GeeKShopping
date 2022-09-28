dd60cc914816094d95a5da41fe7e0fa97388377d
Antonio Henriques <antoniohenriques52@gmail.com>
2022-09-28 20:52:59 -0300

    ConsumerRabbitMQ ok

---
+++ GeekShopping.OrderAPI/Model/Context/SqlContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.OrderAPI.Model.Context
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext() {}
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options) {}
+        public DbSet<OrderDetail> Details { get; set; }
+        public DbSet<OrderHeader> Headers { get; set; }
+            
+    }
+}
--- GeekShopping.OrderAPI/Startup.cs
+++ GeekShopping.OrderAPI/Startup.cs
@@ -1,16 +1,17 @@
+using GeekShopping.OrderAPI.MessageConsumer;
+using GeekShopping.OrderAPI.Model.Context;
+using GeekShopping.OrderAPI.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace GeekShopping.OrderAPI
 {
@@ -26,13 +27,68 @@ namespace GeekShopping.OrderAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SqlContext>(options =>
+                            options.UseSqlServer(Configuration.GetConnectionString("SQLConectionString")));
+
+            var builder = new DbContextOptionsBuilder<SqlContext>();
+            builder.UseSqlServer(Configuration.GetConnectionString("SQLConectionString"));
+
+            services.AddSingleton(new OrderRepository(builder.Options));
+
+            services.AddHostedService<RabbitMQCheckoutConsumer>();
 
             services.AddControllers();
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "https://localhost:4435/";
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = false
+                    };
+                });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ApiScope", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireClaim("scope", "geek_shopping");
+                });
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.OrderAPI", Version = "v1" });
+                c.EnableAnnotations();
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"Enter 'Bearer' [space] and your token!",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In= ParameterLocation.Header
+                        },
+                        new List<string> ()
+                    }
+                });
             });
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -47,7 +103,7 @@ namespace GeekShopping.OrderAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -57,3 +113,4 @@ namespace GeekShopping.OrderAPI
         }
     }
 }
+
